c807ac2b3fe9b484679e59911c4a1bcca5745654
DanPorter <d.g.porter@hotmail.co.uk>
2022-02-21 12:14:14 +0000

    Version 1.0.1 Added option to upload images to parent folder

--- i16_google_logbook_append_babelscan.py
+++ i16_google_logbook_append_babelscan.py
@@ -8,7 +8,7 @@ Requires:
  - a nexus data file
 
 Usage:
-$ python i16_google_logbook_append_image.py /dls_sw/i16/scripts/2022/mm12345-1/mm12345-1.json '12345.nxs'
+$ python i16_google_logbook_append_babelscan.py /dls_sw/i16/scripts/2022/mm12345-1/mm12345-1.json '12345.nxs' fit
 
 By Dan Porter
 Beamline I16
@@ -31,19 +31,25 @@ IMAGE_LOC = 'scan_image.png'
 
 if __name__ == '__main__':
     # --- Command line usage ---
-    filearg = sys.argv[-2]
-    scan_file = sys.argv[-1]
-    if filearg.endswith('.json'):
+    exppar_file = sys.argv[1]
+    scan_file = sys.argv[2]
+    if exppar_file.endswith('.json'):
         if CONFIG:
             i16 = babelscan.instrument_from_config(CONFIG)
             scan = i16.scan(scan_file)
         else:
             scan = babelscan.file_loader(scan_file)
         print(scan)
-        print('Creating plot')
-        fig = scan.plot.detail()
+        if sys.argv[-1] == 'fit':
+            print('Fitting default axes')
+            scan.fit()
+            print('Creating plot')
+            fig = scan.plot.detail(yaxis=['signal', 'fit'])
+        else:
+            print('Creating plot')
+            fig = scan.plot.scananddetector()
         fig.savefig(IMAGE_LOC)
-        append_image(filearg, IMAGE_LOC)
+        append_image(exppar_file, IMAGE_LOC)
     else:
         print('You must enter an experimental parameter file, for example:')
         print(' python i16_google_logbook_append_text.py /dls_sw/i16/scripts/2022/mm12345-1/mm12345-1.json \'file.nxs\'')
